45382330bfadc97266f0d00e3feebcdc2ffb29c8
Banr <61343370+B4nr@users.noreply.github.com>
2023-09-23 18:26:48 -0300

    0.1.4
    i added a change colorscale function. im tryna make it work but ngl it be hard asf
    
    im getting pissed off asf

--- mainPython/grayscale.py
+++ mainPython/grayscale.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy
 
 path = "C:\\Users\\Rodrigo\\Documents\\GitHub\\computerVision\\images\\input\macaVermelha.jpg"
 
@@ -13,17 +14,35 @@ def manualGrayScaleConverterG2G(img, constant):
             img[x,y] = sum(img[x, y]) * constant #this constant better be 1/3
     return img
 
-def manualGrayScaleConverterMe(img):
+b = 3
+g = 0
+r = 0
+
+matrix  = [b, g, r]
+def manualGrayScaleConverterMe(img, cons):
     (row, col) = img.shape[0:2]
-    for  x in range(row):
-        image [x] = image [x] * 0.333
-    for  y in range(col):
-        image [x] = image [x] * 0.333
+    for x in range(row):
+        #numpy.savetxt('r1.txt', img[x])
+        img[x] = img [x] * cons
+        #numpy.savetxt('r2.txt', img[x])
     return img
 
+def generateFrame(leImg, m):
+    #for x in range(0, 100, 1):
+    leImg = manualGrayScaleConverterMe(leImg, [1, x/100, 1])
+    cv2.imwrite(str(m)+".png", leImg)
 
-cv2.imshow('BANRRRR', manualGrayScaleConverterMe(image))
-cv2.waitKey(0)
+for x in range(100, 0, -1):
+    generateFrame(image, x)
+
+
+
+generateFrame(image, 1)
+'''for x in range(1, 2, 1):
+    cv2.imshow('bozo', manualGrayScaleConverterMe(image, [1,0.3,1]))
+    cv2.waitKey(0)
+    print("done", x)'''
+
 
 '''cv2.imshow('Original Image', image)
 cv2.imshow('Grayscale Image', grayImage)
