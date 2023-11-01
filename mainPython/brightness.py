9507273063e3dcd6e50a7b365443914a370950e4
Banr <61343370+B4nr@users.noreply.github.com>
2023-10-31 22:48:13 -0300

    0.1.8
    added pills recognizer

--- mainPython/brightness.py
+++ mainPython/brightness.py
@@ -1,21 +1,40 @@
+import sys
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = "C:\\Users\\Rodrigo\\Documents\\GitHub\\computerVision\\images\\input\\teste.png"
+np.set_printoptions(threshold=sys.maxsize)
+
+path = "C:\\Users\\Rodrigo\\Documents\\GitHub\\computerVision\\images\\input\\tarefaComprimidos\\cart_012.jpg"
 outputPath = "C:\\Users\\Rodrigo\\Documents\\GitHub\\computerVision\\images\\output\\"
 
+onlyFans = cv2.imread(path)
 image = cv2.imread(path)
-img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-threshold_value = 100
+threshold_value = 40
+_, segmentedImage = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
 
-_, segmentedImage = cv2.threshold(img, threshold_value, 255, cv2.THRESH_BINARY)
-
-'''cv2.imshow('Imagem Segmentada', segmented_image)
+'''cv2.imshow('Imagem Segmentada', segmentedImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()'''
 
-plt.imshow(segmentedImage)
+total = [0, 0, 0]
+inverseTotal = ~segmentedImage
 
+(row, col) = segmentedImage.shape[0:2]
+for x in range(row):
+    for y in range(col):
+        total = total + inverseTotal[x,y]
+
+moreTotal = 0
+for x in total:
+    moreTotal = moreTotal+x
+
+trueValue = moreTotal/407581.071429
+
+#print(trueValue)
+
+#plt.imshow(segmentedImage)
+plt.text(1, 35, str("Quantidade de Remedios: "+str(trueValue)))
+plt.imshow(onlyFans)
 plt.show()
